bfbf9e6c13a1aa12e9bafc8fd66a5acec739a115
Tomoki Hayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-08-18 15:42:33 +0900

    updated version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ extras_require = {
 dirname = os.path.dirname(__file__)
 setup(
     name="espnet_model_zoo",
-    version="0.0.0a6",
+    version="0.0.0a7",
     url="http://github.com/espnet/espnet_model_zoo",
     description="ESPnet Model Zoo",
     long_description=open(os.path.join(dirname, "README.md"), encoding="utf-8").read(),
